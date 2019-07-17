76ccb5fa5d4468782da3fe0a1ed26155b7a8dd11
Aswin P Ajayan <aswinp2008@gmail.com>
2019-07-17 20:49:54 +0530

    added graduation rings

--- jarvis.lua
+++ jarvis.lua
@@ -114,7 +114,25 @@ settings_table = {
         end_angle=248
     },
 }
-
+--[[graduation_ring]]
+data={
+    name='grad_jarvis',                    arg='',                  max_value=40,
+    x=296,                          y=652,
+    graph_unit_angle=2.7,          graph_unit_thickness=2.7,
+    graph_bg_colour=0xffffff,      graph_bg_alpha=0.1,
+    graph_fg_colour=0xEF5A29,      graph_fg_alpha=0.5,
+    hand_fg_colour=0xEF5A29,       hand_fg_alpha=0.0,
+    txt_radius=70,
+    txt_weight=10,                  txt_size=9.0,
+    txt_fg_colour=0xEF5A29,        txt_fg_alpha=1.0,
+    graduation_radius=131,
+    graduation_thickness=7,        graduation_mark_thickness=1.0,
+    graduation_unit_angle=10,
+    graduation_fg_colour=0x0, graduation_fg_alpha=0.8,
+    caption='',
+    caption_weight=1,              caption_size=8.0,
+    caption_fg_colour=0xFFFFFF,    caption_fg_alpha=0.3,
+}
 
 
 
@@ -126,6 +144,49 @@ require 'cairo'
 
 function rgb_to_r_g_b(colour,alpha)
     return ((colour / 0x10000) % 0x100) / 255., ((colour / 0x100) % 0x100) / 255., (colour % 0x100) / 255., alpha
+end
+function draw_grad_ring(display, data)
+    local max_value = data['max_value']
+    local x, y = data['x'], data['y']
+    local graph_radius = data['graph_radius']
+    local graph_thickness, graph_unit_thickness = data['graph_thickness'], data['graph_unit_thickness']
+    local graph_unit_angle = data['graph_unit_angle']
+    local graph_bg_colour, graph_bg_alpha = data['graph_bg_colour'], data['graph_bg_alpha']
+    local graph_fg_colour, graph_fg_alpha = data['graph_fg_colour'], data['graph_fg_alpha']
+
+
+    local i = 1
+    local angle = (graph_unit_angle * i) - graph_unit_thickness
+
+    -- graduations marks
+    local graduation_radius = data['graduation_radius']
+    local graduation_thickness, graduation_mark_thickness = data['graduation_thickness'], data['graduation_mark_thickness']
+    local graduation_unit_angle = data['graduation_unit_angle']
+    local graduation_fg_colour, graduation_fg_alpha = data['graduation_fg_colour'], data['graduation_fg_alpha']
+    if graduation_radius > 0 and graduation_thickness > 0 and graduation_unit_angle > 0 then
+        local nb_graduation = 160 / graduation_unit_angle
+        local i = 1
+        while i <= nb_graduation do
+            cairo_set_line_width(display, graduation_thickness)
+            cairo_arc(display, x, y, graduation_radius, (((graduation_unit_angle * i)-(graduation_mark_thickness/2)-160)*(2*math.pi/360))-(math.pi/4),(((graduation_unit_angle * i)+(graduation_mark_thickness/2)-160)*(2*math.pi/360))-(math.pi/4))
+            cairo_set_source_rgba(display,rgb_to_r_g_b(graduation_fg_colour,graduation_fg_alpha))
+            cairo_stroke(display)
+            cairo_set_line_width(display, graph_thickness)
+            i = i + 1
+        end
+    end
+cairo_set_line_width(display, 1)
+cairo_set_source_rgba(display,rgb_to_r_g_b(graduation_fg_colour,graduation_fg_alpha))
+cairo_arc(display, x, y, graduation_radius -3, -200*2*math.pi/360,-48*2*math.pi/360)
+cairo_stroke(display)
+cairo_set_line_width(display, 1)
+cairo_set_source_rgba(display,rgb_to_r_g_b(graduation_fg_colour,graduation_fg_alpha))
+cairo_arc(display, x, y, graduation_radius -5, -200*2*math.pi/360,-48*2*math.pi/360)
+cairo_stroke(display)
+cairo_set_line_width(display, 1)
+cairo_set_source_rgba(display,rgb_to_r_g_b(graduation_fg_colour,graduation_fg_alpha))
+cairo_arc(display, x, y, graduation_radius -20, -200*2*math.pi/360,-48*2*math.pi/360)
+cairo_stroke(display)
 end
 
 function draw_ring(cr,t,pt)
@@ -182,6 +243,7 @@ function conky_clock_rings()
         for i in pairs(settings_table) do
             setup_rings(cr,settings_table[i])
         end
+	draw_grad_ring(cr,data)
     end
     
 	cairo_destroy(cr)
